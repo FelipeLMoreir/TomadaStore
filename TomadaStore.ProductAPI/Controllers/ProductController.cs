6b39d3c2d10eba895530f921d0c6caf42a2f779a
FelipeLMoreir <felipe.lm.moreira@gmail.com>
2025-12-04 09:52:28 -0300

    service

---
+++ TomadaStore.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TomadaStore.ProductAPI.Services.Interfaces;
+
+namespace TomadaStore.ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly IProductService _productService;
+    }
+}
---
+++ TomadaStore.ProductAPI/Services/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿]using TomadaStore.Models.DTOs.Product;
+
+namespace TomadaStore.ProductAPI.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<List<ProductResponseDTO>> GetProductAsync();
+        Task<ProductResponseDTO> GetProductByIDAsync(string id);
+        Task CreateProductAsync(ProductRequestDTO product);
+        Task UpdateProductAsync(string id, ProductRequestDTO productDto);
+        Task DeleteProductAsync(string id);
+    }
+}

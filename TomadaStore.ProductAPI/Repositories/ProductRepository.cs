03bc3b78ef47805f07f26f2fcacaf0f71b236be9
FelipeLMoreir <felipe.lm.moreira@gmail.com>
2025-12-04 11:07:00 -0300

    repository connection e create

--- TomadaStore.ProductAPI/Repositories/ProductRepository.cs
+++ TomadaStore.ProductAPI/Repositories/ProductRepository.cs
@@ -1,12 +1,44 @@
-﻿namespace TomadaStore.ProductAPI.Repositories
+﻿using MongoDB.Driver;
+using TomadaStore.CustomerAPI.Data;
+using TomadaStore.Models.DTOs.Product;
+using TomadaStore.Models.Models;
+using TomadaStore.ProductAPI.Repositories.Interfaces;
+
+namespace TomadaStore.ProductAPI.Repositories
 {
-    public class ProductRepository
+    public class ProductRepository: IProductRepository
     {
         private readonly ILogger<ProductRepository> _logger;
-        private readonly 
-        public ProductRepository(ILogger<ProductRepository> logger)
+        private readonly IMongoCollection<Product> _mongoCollection;
+        private readonly ConnectionDB _connection;
+        public ProductRepository(ILogger<ProductRepository> logger, ConnectionDB connectionDB)
         {
             _logger = logger;
+            _connection = connectionDB;
+            _mongoCollection = _connection.GetMongoCollection();
+        }
+
+        public async Task CreateProductAsync(ProductRequestDTO productDTO)
+        {
+            try
+            {
+                await _mongoCollection.InsertOneAsync(new Product(
+                    productDTO.Name,
+                    productDTO.Description,
+                    productDTO.Price,
+                    productDTO.Category
+                ));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("erro" + ex.Message);
+                throw;
+            }
+        }
+
+        public Task<List<ProductResponseDTO>> GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
         }
     }
 }
